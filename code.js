d5d091dd8f20abeb05f1538014c1a57158dd9f32
Andon Mefford <andonmefford@outlook.com>
2023-10-23 20:17:28 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
 function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+    const visited = new Set();
+    const path = [];
+    function dfs(node) {
+      if (node === targetNode) {
+        path.push(node);
+        return true;
+      }
+      visited.add(node);
+      path.push(node);
+      for (const neighbor of graph[node]) {
+        if (!visited.has(neighbor) && dfs(neighbor)) {
+          return true;
+        }
+      }
+      path.pop();
+      return false;
+    }
+    if (dfs(startNode)) {
+      return path;
+    } else {
+      return [];
+    }
 }
